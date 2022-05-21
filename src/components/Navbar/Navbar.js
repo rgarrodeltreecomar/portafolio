2cd341ea78b47f6e84ebaf32c71447bda59fe009
Rafa Tárrega <rafatarre@gmail.com>
2022-05-21 16:56:43 +0200

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./Navbar.styles";
+
+const Navbar = () => {
+  return <S.ContainerNavbar>Nuestro container navabr</S.ContainerNavbar>;
+};
+
+export default Navbar;
