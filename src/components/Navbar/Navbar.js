515e28798047ce5f8c720dff8d5f62ba9b32a9f5
Rafa Tárrega <rafatarre@gmail.com>
2022-05-21 17:40:34 +0200

    feat: add burguer menu mobile and desktop

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./Navbar.styles";
 
 const Navbar = () => {
-  return <S.ContainerNavbar>Nuestro container navabr</S.ContainerNavbar>;
+  const [showMenuBurguer, setShowMenuBurguer] = useState(false);
+
+  return (
+    <S.ContainerNavbar>
+      <S.ContainerLogo>
+        <S.ImageLogo src="/img/logo.png" alt="Logo Rafa Tárrega" />
+        <div>
+          <h1>Rafa Tárrega</h1>
+          <h2>Frontend Developer</h2>
+        </div>
+      </S.ContainerLogo>
+
+      <S.ContainerBurguer onClick={() => setShowMenuBurguer(!showMenuBurguer)}>
+        <S.BurguerMenu />
+        <S.BurguerMenu />
+        <S.BurguerMenu />
+      </S.ContainerBurguer>
+
+      <S.ContainerItems showMenuBurguer={showMenuBurguer}>
+        <p>About me</p>
+        <p>Projects</p>
+        <S.Button>🤙 Contact me</S.Button>
+      </S.ContainerItems>
+    </S.ContainerNavbar>
+  );
 };
 
 export default Navbar;
