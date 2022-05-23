6668dd2f66a5e4b357e134038fcf68c667db9739
Rafa Tárrega <rafatarre@gmail.com>
2022-05-23 23:42:20 +0200

    feat: add projects container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Container from "./components/Container/Container";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Container>
         <AboutMe />
+        <Projects />
       </Container>
     </>
   );
